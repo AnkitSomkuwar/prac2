782829d5d5de545db19e21f021686507cd33dd15
AnkitSomkuwar <146408398+AnkitSomkuwar@users.noreply.github.com>
2024-02-05 22:35:33 +0530

    Update index.js

--- src/components/CardItem/index.js
+++ src/components/CardItem/index.js
@@ -8,6 +8,18 @@ const TechCardItem = props => {
   return (
     <div className="tech-bg-container">
       <h1 className="main-heading">Learn 4.0 Technologies</h1>
+      <p className="main-description">
+        Get trained by alumni of IITs and top companies like Amazon, Microsoft,
+        Intel, Nvidia, Qualcomm, etc. Learn directly from professionals involved
+        in Product Development.
+      </p>
+      <div className="bg-card-container">
+        <div className="card-1">
+          <h1 className="card-heading">{title}</h1>
+          <p className="card-description">{description}</p>
+          <img className="card-img" alt={title} src={imgUrl} />
+        </div>
+      </div>
     </div>
   )
 }
